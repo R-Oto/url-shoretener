378ee90dce0e83793f9bd49e48ab634ca13cb708
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-12 15:52:27 +0400

    setup

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server started"))
